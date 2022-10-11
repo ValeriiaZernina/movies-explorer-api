7e99b326f16f37f85cca834575edbdec0158eb09
Valeria Zernina <darovanie@mail.ru>
2022-10-11 19:12:00 +0500

    env

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const { errors } = require('celebrate');
-const cors = require('cors');
-const { requestLogger, errorLogger } = require('./middleware/logger');
-const { handleError } = require('./utils/handleError');
-const router = require('./routes/index');
-const { MONGO_DEV_URL } = require('./utils/config');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const { errors } = require("celebrate");
+const cors = require("cors");
+const { requestLogger, errorLogger } = require("./middleware/logger");
+const { handleError } = require("./utils/handleError");
+const router = require("./routes/index");
 // запуск на 3000 порту
-const { PORT = 3000, NODE_ENV, MONGO_URL } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
 // подключаемся к серверу mongo
-mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : MONGO_DEV_URL, {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
 });
 
@@ -25,14 +24,14 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      'https://api.zernina.nomoredomains.icu',
-      'https://zernina.nomoredomains.icu',
-      'http://zernina.nomoredomains.icu',
-      'http://localhost:3001',
-      'http://localhost:3000',
+      "https://api.zernina.nomoredomains.icu",
+      "https://zernina.nomoredomains.icu",
+      "http://zernina.nomoredomains.icu",
+      "http://localhost:3001",
+      "http://localhost:3000",
     ],
     credentials: true,
-  }),
+  })
 );
 // подключаем роуты
 app.use(router);
--- utils/config.js
+++
@@ -1,5 +0,0 @@
-const MONGO_DEV_URL = 'mongodb://localhost:27017/diplom';
-
-module.exports = {
-  MONGO_DEV_URL,
-};
