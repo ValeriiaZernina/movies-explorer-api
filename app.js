392f34e2f646d64e4a8cf66c4b19e3aa5d097063
Valeria Zernina <darovanie@mail.ru>
2022-10-11 19:14:13 +0500

    eslint

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
-const { errors } = require("celebrate");
-const cors = require("cors");
-const { requestLogger, errorLogger } = require("./middleware/logger");
-const { handleError } = require("./utils/handleError");
-const router = require("./routes/index");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const cors = require('cors');
+const { requestLogger, errorLogger } = require('./middleware/logger');
+const { handleError } = require('./utils/handleError');
+const router = require('./routes/index');
 // запуск на 3000 порту
 const { PORT = 3000 } = process.env;
 
@@ -24,14 +24,14 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://api.zernina.nomoredomains.icu",
-      "https://zernina.nomoredomains.icu",
-      "http://zernina.nomoredomains.icu",
-      "http://localhost:3001",
-      "http://localhost:3000",
+      'https://api.zernina.nomoredomains.icu',
+      'https://zernina.nomoredomains.icu',
+      'http://zernina.nomoredomains.icu',
+      'http://localhost:3001',
+      'http://localhost:3000',
     ],
     credentials: true,
-  })
+  }),
 );
 // подключаем роуты
 app.use(router);
--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-const bcrypt = require("bcrypt"); // импортируем bcrypt
-const jwt = require("jsonwebtoken");
-const userModel = require("../models/user");
-const { StatusBadRequest } = require("../utils/errors/StatusBadRequest");
-const { StatusNotFound } = require("../utils/errors/StatusNotFound");
-const { ConflictError } = require("../utils/errors/ConflictError");
-const { STATUS_CREATED, STATUS_OK } = require("../utils/errors/errorsCode");
+require('dotenv').config();
+const bcrypt = require('bcrypt'); // импортируем bcrypt
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/user');
+const { StatusBadRequest } = require('../utils/errors/StatusBadRequest');
+const { StatusNotFound } = require('../utils/errors/StatusNotFound');
+const { ConflictError } = require('../utils/errors/ConflictError');
+const { STATUS_CREATED, STATUS_OK } = require('../utils/errors/errorsCode');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -14,28 +14,24 @@ module.exports.createUser = (req, res, next) => {
   const { name, email, password } = req.body;
   bcrypt
     .hash(password, 10)
-    .then((hash) =>
-      userModel.create({
-        name,
-        email,
-        password: hash, // записываем хеш в базу
-      })
-    )
-    .then((user) =>
-      res.status(STATUS_CREATED).send({
-        name: user.name,
-        email: user.email,
-        _id: user._id,
-      })
-    )
+    .then((hash) => userModel.create({
+      name,
+      email,
+      password: hash, // записываем хеш в базу
+    }))
+    .then((user) => res.status(STATUS_CREATED).send({
+      name: user.name,
+      email: user.email,
+      _id: user._id,
+    }))
     .catch((err) => {
       if (err.code === 11000) {
-        next(new ConflictError("Пользователь уже существует"));
-      } else if (err.name === "ValidationError") {
+        next(new ConflictError('Пользователь уже существует'));
+      } else if (err.name === 'ValidationError') {
         next(
           new StatusBadRequest(
-            "Переданы некорректные данные при создании пользователя пользователя"
-          )
+            'Переданы некорректные данные при создании пользователя пользователя',
+          ),
         );
       } else {
         next(err);
@@ -58,15 +54,15 @@ module.exports.patchUserMe = (req, res, next) => {
     .findByIdAndUpdate(
       req.user._id,
       { name, email },
-      { new: true, runValidators: true }
+      { new: true, runValidators: true },
     )
     .orFail(() => {
       throw new StatusNotFound(`Пользователь с id=${req.user._id} не найден`);
     })
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === "ValidationError") {
-        next(new StatusBadRequest("Переданы некорректные данные"));
+      if (err.name === 'ValidationError') {
+        next(new StatusBadRequest('Переданы некорректные данные'));
       } else {
         next(err);
       }
@@ -81,32 +77,32 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === "production" ? JWT_SECRET : "SECRET",
+        NODE_ENV === 'production' ? JWT_SECRET : 'SECRET',
         {
           expiresIn: 1000 * 60 * 60 * 24 * 7,
-        }
+        },
       );
       res
         .cookie(
-          "jwt",
+          'jwt',
           token, // token - наш JWT токен, который мы отправляем
           {
             maxAge: 3600000 * 24 * 7,
             httpOnly: true,
             sameSite: false,
-          }
+          },
         )
-        .send({ message: "Пользователь успешно вошел в систему" });
+        .send({ message: 'Пользователь успешно вошел в систему' });
     })
     .catch((err) => {
       // очищаем куку при неудачном логине
-      res.clearCookie("jwt");
+      res.clearCookie('jwt');
       next(err); // пробрасываем ошибку дальше в центральный обработчик
     });
 };
 
 module.exports.signout = (req, res) => {
-  res.clearCookie("jwt").send({
+  res.clearCookie('jwt').send({
     message: `Пользователь с id=${req.user._id} успешно вышел из системы`,
   });
 };
